eaee2df5fb5a2646683116d8d948b9e2afcc5a3f
harraz <harraz@gmail.com>
2025-12-22 01:29:57 -0500

    Update MQTT_MAX_PACKET_SIZE to 1024 and reorder includes in main.cpp and globals.h

--- elghaffar-sensor-hub/elghafeer/src/main.cpp
+++ elghaffar-sensor-hub/elghafeer/src/main.cpp
@@ -1,6 +1,5 @@
 #include <ESP8266WiFi.h>
 #include "globals.h"
-#include <PubSubClient.h>
 #include "secrets.h"   // #define WIFI_SSID, WIFI_PASSWORD
 #include "handlecmds.h"
 #include <ArduinoJson.h>

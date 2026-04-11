a2d72b044dab81120541be0cdf286bfc023ac304
harraz <harraz@gmail.com>
2026-04-10 22:52:16 -0400

    Make regular branch settings example device-agnostic

--- elghaffar-sensor-hub/elghafeer/src/settings.example.h
+++ elghaffar-sensor-hub/elghafeer/src/settings.example.h
@@ -4,7 +4,7 @@
 // Copy this file to settings.h and change the values for the device you are
 // flashing. Keep settings.h local so one branch can be reused for many devices.
 
-#define DEVICE_GHAFEER_NAME "ASHRAF"
+#define DEVICE_GHAFEER_NAME "CHANGE_ME"
 #define MQTT_BROKER_HOST "192.168.1.246"
 #define MQTT_BROKER_PORT 1883
 #define DEFAULT_DEBUG false
